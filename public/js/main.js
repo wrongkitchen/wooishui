15db3f9c8c78dfddb1531d44fc76bae1bfaa1ffb
wrongkitchen <wkc.kenji@gmail.com>
2015-02-11 11:58:41 +0800

    added main page refresh handler

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,10 @@ require(["FacebookHelper", "PopupFriendList", "DebtsCredits"], function(fbh, pfl
 			else
 				$("#header").removeClass();
 		});
+		$$("#homeDebts").on("refresh", function(e){
+			_sgd.debtsCredits.userRefresh = true;
+			_sgd.debtsCredits.credits.fetch();
+		});
 		_sgd.popupFriendList = new pfl({
 			wrapper: "#friendList",
 			inviteTarget: "#nonRegFriend",
