18e5b57ffba70fbf808d20d43e8c768995904d07
wrongkitchen <wkc.kenji@gmail.com>
2015-02-15 13:06:29 +0800

    change debtType UI

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,42 @@ require(["FacebookHelper", "PopupFriendList", "DebtsCredits"], function(fbh, pfl
 
 	var $$ = Dom7;
 	var _sgd = (window.sgd) ? window.sgd : {};
-		_sgd.framework7 = new Framework7();
-		_sgd.framework7.onPageBeforeAnimation('*', function(page,a,b,c){
+		_sgd.framework7 = new Framework7({
+			pushState: true
+		});
+		var mainView = _sgd.framework7.addView('.view-main', {
+			dynamicNavbar: true,
+			domCache: true
+		});
+		_sgd.changeSection = function(pPath){
+			if(pPath == 'form-second'){
+				if($('#otherUserID').val() === "" && $('#otherUserName').val() === ''){
+					_sgd.framework7.alert('Please enter a name / select a wooishui user', ['Name missing']);
+					return false;
+				}
+			} else if(pPath == 'home'){
+				$('#otherUserID').val('');
+				$('#otherUserName').val('');
+				$('#debtForm')[0].reset();
+			}
+			mainView.router.load({ pageName: pPath });
+		};
+		_sgd.framework7.onPageBeforeAnimation('*', function(page){
 			if(page.container.dataset.nav)
 				$("#header").addClass(page.container.dataset.nav);
 			else
 				$("#header").removeClass();
+			if(page.name == "form-second"){
+				if($("#otherUserID").val()){
+					$(".debtType .right").html("<img src=\"http://graph.facebook.com/" + $("#otherUserID").val() + "/picture\" alt=\"\" />");
+				} else if($("#otherUserName").val()){
+					$(".debtType .right").html('<span>' + $("#otherUserName").val() + '</span>');
+				} else {
+					_sgd.framework7.alert('Please enter a name / select a wooishui user', function(){
+						_sgd.changeSection('form');
+					});
+				}
+			}
 		});
 		_sgd.popupFriendList = new pfl({
 			wrapper: "#friendList",
@@ -27,10 +57,8 @@ require(["FacebookHelper", "PopupFriendList", "DebtsCredits"], function(fbh, pfl
 			_sgd.debtsCredits.credits.fetch();
 		});
 		_sgd.submitDebt = function(){
-			var isCreator = $(".debtTypeBtn.active[data-role=subject]").data('value') === 'currentUser';
-			var isDebt = $(".debtTypeBtn.active[data-role=verb]").data('value') === 'own';
 			var _q = {
-				isCreatorDebt: (isCreator) ? isDebt : !isDebt,
+				isCreatorDebt: $(".debtType .middle").hasClass('creatorDebt'),
 				price: parseFloat($("#debtForm input[name=price]").val()) || 0,
 				desc: $("#debtForm input[name=desc]").val(),
 				otherUserID: $("#otherUserID").val(),
@@ -52,25 +80,6 @@ require(["FacebookHelper", "PopupFriendList", "DebtsCredits"], function(fbh, pfl
 				sgd.framework7.alert('Please fill in an amount', ['Amount missing']);
 			}
 		};
-
-	var mainView = _sgd.framework7.addView('.view-main', {
-		dynamicNavbar: true,
-		domCache: true
-	});
-	_sgd.changeSection = function(pPath){
-		if(pPath == 'form-second'){
-			if($('#otherUserID').val() === "" && $('#otherUserName').val() === ''){
-				_sgd.framework7.alert('Please enter a name / select a wooishui user', ['Name missing']);
-				return false;
-			}
-		} else if(pPath == 'home'){
-			$('#otherUserID').val('');
-			$('#otherUserName').val('');
-			$('#debtForm')[0].reset();
-		}
-		mainView.router.load({ pageName: pPath });
-	};
-
 	$$('.menu-link').on('click', function () {
     	var buttons = [
 	        {
@@ -82,20 +91,31 @@ require(["FacebookHelper", "PopupFriendList", "DebtsCredits"], function(fbh, pfl
 	    ];
 	    _sgd.framework7.actions(buttons);
 	});   
-
-	$('.debtTypeBtn').on('click', function(){
-		if($(this).hasClass('active')) return false;
-		var targetRole = $(this).data('role');
-		var targetValue = $(this).data('value');
-		$('.debtTypeBtn[data-role='+ targetRole +']').removeClass('active');
-		$(this).addClass('active');
-		if(targetRole != 'verb'){
-			var otherRole = '.debtTypeBtn[data-role='+ ((targetRole == 'subject') ? 'object' : 'subject') + ']';
-			var otherValue = '[data-value='+ ((targetValue == 'otherUser') ? 'currentUser' : 'otherUser') + ']';
-			$(otherRole).removeClass('active');
-			$(otherRole+otherValue).addClass('active');
-		}
+	$(".debtType .left").on('click', function(){
+		$(".debtType .middle").addClass('creatorDebt');
 	});
+	$(".debtType .right").on('click', function(){
+		$(".debtType .middle").removeClass('creatorDebt');
+	});
+	$(".debtType .middle").on('click', function(){
+		if($(this).hasClass('creatorDebt'))
+			$(this).removeClass('creatorDebt');
+		else 
+			$(this).addClass('creatorDebt');
+	});
+	// $('.debtTypeBtn').on('click', function(){
+	// 	if($(this).hasClass('active')) return false;
+	// 	var targetRole = $(this).data('role');
+	// 	var targetValue = $(this).data('value');
+	// 	$('.debtTypeBtn[data-role='+ targetRole +']').removeClass('active');
+	// 	$(this).addClass('active');
+	// 	if(targetRole != 'verb'){
+	// 		var otherRole = '.debtTypeBtn[data-role='+ ((targetRole == 'subject') ? 'object' : 'subject') + ']';
+	// 		var otherValue = '[data-value='+ ((targetValue == 'otherUser') ? 'currentUser' : 'otherUser') + ']';
+	// 		$(otherRole).removeClass('active');
+	// 		$(otherRole+otherValue).addClass('active');
+	// 	}
+	// });
 	$('#menu a.internal').on('click', function(){
 		_sgd.changeSection($(this).attr('href'));
 		_sgd.framework7.closePanel();
